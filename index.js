d1bd0278d6024d8b616253a7d332c78b0a042a32
Yehonathan Sharvit <viebel@gmail.com>
2019-08-19 16:41:37 +0300

    /google-drive-change endpoint

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+    .get('/google-drive-change', (req, res) => {
+        console.log(`received a google drive change: ${req}\n ${req.body}`);
+        res.send('Got the info');
+    })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
